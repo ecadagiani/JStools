de180f141d10a1207eb8d1c6052e1925064ee2fe
Eden <e.cadagiani@gmail.com>
2019-11-17 15:13:44 +0100

    add combination

--- functions/functions.js
+++ functions/functions.js
@@ -1,21 +1,21 @@
-const {URL} = require("url");
+const { URL } = require( "url" );
 const {
     transform, isEqual, concat, isObject, get
-} = require("lodash");
+} = require( "lodash" );
 
 
-function makeTree(obj, key, acc = []){
-    const keyArray = Object.keys(obj);
-    for (let i = 0; i < keyArray.length; i++) {
+function makeTree( obj, key, acc = [] ) {
+    const keyArray = Object.keys( obj );
+    for ( let i = 0; i < keyArray.length; i++ ) {
         const itKey = keyArray[i];
-        if (itKey === key) {
-            acc.push(itKey);
+        if ( itKey === key ) {
+            acc.push( itKey );
             return acc;
         }
 
-        if (obj[itKey] && typeof obj[itKey] === "object") {
-            const foo = makeTree(obj[itKey], key, concat(acc, [itKey]));
-            if (Array.isArray(foo))
+        if ( obj[itKey] && typeof obj[itKey] === "object" ) {
+            const foo = makeTree( obj[itKey], key, concat( acc, [itKey] ) );
+            if ( Array.isArray( foo ) )
                 return foo;
         }
     }
@@ -23,44 +23,44 @@ function makeTree(obj, key, acc = []){
 }
 
 
-function difference(object, base) {
-    function changes(object, base) {
-        return transform(object, function (result, value, key) {
-            if (!isEqual(value, base[key])) {
-                result[key] = (isObject(value) && isObject(base[key])) ? changes(value, base[key]) : value;
+function difference( object, base ) {
+    function changes( object, base ) {
+        return transform( object, function ( result, value, key ) {
+            if ( !isEqual( value, base[key] ) ) {
+                result[key] = (isObject( value ) && isObject( base[key] )) ? changes( value, base[key] ) : value;
             }
-        });
+        } );
     }
 
-    return changes(object, base);
+    return changes( object, base );
 }
 
 
-function getPathsKey(obj, keySearch) {
+function getPathsKey( obj, keySearch ) {
     let paths = [];
     let nodes = [{
         obj,
         path: [],
     }];
-    while (nodes.length > 0) {
+    while ( nodes.length > 0 ) {
         let n = nodes.pop();
-        Object.keys(n.obj).forEach(k => {
-            let path = n.path.concat(k);
-            if (typeof n.obj[k] === "object") {
+        Object.keys( n.obj ).forEach( k => {
+            let path = n.path.concat( k );
+            if ( typeof n.obj[k] === "object" ) {
 
-                if (k === keySearch) {
-                    paths.push(path.join("."));
+                if ( k === keySearch ) {
+                    paths.push( path.join( "." ) );
                 }
-                nodes.unshift({
+                nodes.unshift( {
                     obj: n.obj[k],
                     path: path
-                });
+                } );
             } else {
-                if (k === keySearch) {
-                    paths.push(path.join("."));
+                if ( k === keySearch ) {
+                    paths.push( path.join( "." ) );
                 }
             }
-        });
+        } );
     }
     return paths;
 }
@@ -73,47 +73,47 @@ function getPathsKey(obj, keySearch) {
  * @param {function=} iterate - A function to know if a value must or not be added to the array
  * @return {array}
  */
-function valuesInDeep(object, iterate = null){
-    if(typeof object !== "object") return;
+function valuesInDeep( object, iterate = null ) {
+    if ( typeof object !== "object" ) return;
 
     let res = [];
-    Object.keys(object).forEach(key => {
-        if(typeof object[key] === "object")
-            res = res.concat( valuesInDeep(object[key], iterate) );
+    Object.keys( object ).forEach( key => {
+        if ( typeof object[key] === "object" )
+            res = res.concat( valuesInDeep( object[key], iterate ) );
 
-        if((typeof iterate === "function" && iterate(object[key], key)) || !iterate)
-            res.push(object[key]);
-    });
+        if ( (typeof iterate === "function" && iterate( object[key], key )) || !iterate )
+            res.push( object[key] );
+    } );
     return res;
 }
 
 
-function wait(ms){
-    return new Promise(r=>setTimeout(r, ms));
+function wait( ms ) {
+    return new Promise( r => setTimeout( r, ms ) );
 }
 
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+function getRndInteger( min, max ) {
+    return Math.floor( Math.random() * (max - min + 1) ) + min;
 }
 
 
-function getUrlParts(url) {
+function getUrlParts( url ) {
     let parsedUrl;
-    try{
-        parsedUrl = new URL(url);
-    }catch(err) {
+    try {
+        parsedUrl = new URL( url );
+    } catch ( err ) {
         return null;
     }
 
-    const domainResReg = (/[\w-]+\.(\w+|(co|com)\.\w+)$/gm).exec(parsedUrl.hostname);
-    const extensionRegRes = (/\.(\w+$)/gm).exec(parsedUrl.pathname);
-    const uriSchemeRegRes = (/[\w-]+/gm).exec(parsedUrl.protocol);
+    const domainResReg = (/[\w-]+\.(\w+|(co|com)\.\w+)$/gm).exec( parsedUrl.hostname );
+    const extensionRegRes = (/\.(\w+$)/gm).exec( parsedUrl.pathname );
+    const uriSchemeRegRes = (/[\w-]+/gm).exec( parsedUrl.protocol );
 
     return {
-        uriScheme: get(uriSchemeRegRes, "[0]"),
-        extension: get(extensionRegRes, "[1]"),
-        domain: get(domainResReg, "[0]"),
+        uriScheme: get( uriSchemeRegRes, "[0]" ),
+        extension: get( extensionRegRes, "[1]" ),
+        domain: get( domainResReg, "[0]" ),
         pathname: parsedUrl.pathname,
         hostname: parsedUrl.hostname,
         port: parsedUrl.port,
@@ -127,13 +127,13 @@ function getUrlParts(url) {
  * @param numberOfDraw
  * @return {[]}
  */
-function drawWithoutDuplicate(values, numberOfDraw) {
+function drawWithoutDuplicate( values, numberOfDraw ) {
     const res = [];
-    const arrayToDraw = Array.from(values);
-    for(let i = 0; i < numberOfDraw && arrayToDraw.length > 0 ; i++) {
-        const index = getRndInteger( 0, arrayToDraw.length - 1);
+    const arrayToDraw = Array.from( values );
+    for ( let i = 0; i < numberOfDraw && arrayToDraw.length > 0; i++ ) {
+        const index = getRndInteger( 0, arrayToDraw.length - 1 );
         res[i] = arrayToDraw[index];
-        arrayToDraw.splice(index, 1);
+        arrayToDraw.splice( index, 1 );
     }
     return res;
 }
@@ -144,23 +144,23 @@ function drawWithoutDuplicate(values, numberOfDraw) {
  * @param updater
  * @return {Object}
  */
-function updateValuesInDeep ( object, updater = null ) {
-    if( typeof object !== "object" ) return object;
+function updateValuesInDeep( object, updater = null ) {
+    if ( typeof object !== "object" ) return object;
 
     let obj = {};
     Object.keys( object ).forEach( key => {
-        if( Array.isArray( object[key])) {
+        if ( Array.isArray( object[key] ) ) {
             obj[key] = [];
-            object[key].forEach(( item, index ) => {
-                obj[key].push( updateValuesInDeep( item, updater ));
-            });
-        }else if( typeof object[key] === "object" )
+            object[key].forEach( ( item, index ) => {
+                obj[key].push( updateValuesInDeep( item, updater ) );
+            } );
+        } else if ( typeof object[key] === "object" )
             obj[key] = updateValuesInDeep( object[key], updater );
-        else if( typeof updater === "function" )
-            obj[key] = updater( object[key]);
+        else if ( typeof updater === "function" )
+            obj[key] = updater( object[key] );
         else
             obj[key] = object[key];
-    });
+    } );
     return obj;
 }
 
@@ -170,13 +170,13 @@ function updateValuesInDeep ( object, updater = null ) {
  * @param sortFunction
  * @return {Object}
  */
-function sortObjectByKey ( obj, sortFunction = undefined ) {
+function sortObjectByKey( obj, sortFunction = undefined ) {
     const ordered = {};
     Object.keys( obj )
-        .sort(sortFunction)
+        .sort( sortFunction )
         .forEach( key => {
             ordered[key] = obj[key];
-        });
+        } );
     return ordered;
 }
 
@@ -185,10 +185,35 @@ function sortObjectByKey ( obj, sortFunction = undefined ) {
  * @param nbDays
  * @return {Date}
  */
-export function getDateWithDay ( nbDays ) {
+export function getDateWithDay( nbDays ) {
     const date = new Date();
     date.setDate( new Date().getDate() + nbDays );
     return date;
+}
+
+/**
+ * Combine array of string with all possibility
+ * @param {Array<string>} data - all string
+ * @param {function} onCombine - function call on each new combine
+ * @return {[]}
+ */
+function combinations( data, onCombine ) {
+    const result = [];
+    const fn = function ( prefix, data ) {
+        for ( let i = 0; i < data.length; i++ ) {
+            const newPrefix = prefix + data[i];
+            const newData = Array.from( data );
+            newData.splice( i, 1 );
+            if ( !result.includes( newPrefix ) ) {
+                result.push( newPrefix );
+                if ( typeof onCombine === "function" )
+                    onCombine( newPrefix );
+            }
+            fn( newPrefix, newData );
+        }
+    };
+    fn( "", data );
+    return result;
 }
 
 
@@ -204,4 +229,5 @@ module.exports = {
     updateValuesInDeep,
     sortObjectByKey,
     getDateWithDay,
+    combinations
 };
